59bbe2698e6b6b99056195f2de0c741470b4c64a
Cengizhan Tomak <cengizhantomak@gmail.com>
2023-03-09 13:01:02 +0300

    Kusu Ucurma

--- AngryBirdCloneSpriteKit/GameScene.swift
+++ AngryBirdCloneSpriteKit/GameScene.swift
@@ -42,9 +42,9 @@ class GameScene: SKScene {
         let birdTexture = SKTexture(imageNamed: "bird")
         
         bird.physicsBody = SKPhysicsBody(circleOfRadius: birdTexture.size().height / 13)
-        bird.physicsBody?.affectedByGravity = true
+        bird.physicsBody?.affectedByGravity = false
         bird.physicsBody?.isDynamic = true
-        bird.physicsBody?.mass = 0.5
+        bird.physicsBody?.mass = 0.15
         
         //Box
         let boxTexture = SKTexture(imageNamed: "brick")
@@ -101,6 +101,9 @@ class GameScene: SKScene {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        bird.physicsBody?.applyImpulse(CGVector(dx: 50, dy: 100))
+        bird.physicsBody?.affectedByGravity = true
         
     }
     
